2ce9d1a0a879f8a2f879557cfedab6714f4e5aa6
Hyangan Lee <thatmoment@naver.com>
2017-04-22 12:08:58 +0000

    Update BMI program

--- bmi_v2.py
+++
@@ -1,6 +0,0 @@
-height = input('신장을 입력하세요(cm) : ')
-weight = input('체중을 입력하세요(kg) : ')
-bmi = float(weight) / float(height) ** 2 * 10000
-print ('나의 신체질량지수(bmi) {:.2f}'.format(bmi))
-if 23 < bmi :
-    print ('당신은 과체중 입니다')
